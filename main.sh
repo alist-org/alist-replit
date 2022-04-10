91574017375f62c6e45e72252ad2f55a4872027e
Xhofe <i@nn.ci>
2022-04-10 21:48:08 +0800

    remove proxy

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 rm -rf alist* data/
-curl -L https://ghproxy.com/https://github.com/Xhofe/alist/releases/latest/download/alist-linux-amd64.tar.gz -o alist.tar.gz
+curl -L https://github.com/Xhofe/alist/releases/latest/download/alist-linux-amd64.tar.gz -o alist.tar.gz
 tar -zxvf alist.tar.gz
 mv alist-linux-amd64 alist
 ./alist -docker
