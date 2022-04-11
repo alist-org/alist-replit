6fb9ded0638c97ca3549f08498fb2f15e6b9f778
Xhofe <i@nn.ci>
2022-04-11 22:39:53 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,8 @@
-rm -rf alist* data/
-curl -L https://github.com/Xhofe/alist/releases/latest/download/alist-linux-amd64.tar.gz -o alist.tar.gz
-tar -zxvf alist.tar.gz
-mv alist-linux-amd64 alist
+# rm -rf alist* data/ #Uncomment this line to update
+if [ ! -f "alist" ];then
+  curl -L https://github.com/Xhofe/alist/releases/latest/download/alist-linux-amd64.tar.gz -o alist.tar.gz
+  tar -zxvf alist.tar.gz
+  rm -f alist.tar.gz
+  mv alist-linux-amd64 alist
+fi
 ./alist -docker
