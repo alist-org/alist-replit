da058cbd76e1d00c6174953205dc497adb55dd99
Xhofe <i@nn.ci>
2022-04-18 15:12:59 +0800

    switch to musl

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 # rm -rf alist* data/ #Uncomment this line to update
 if [ ! -f "alist" ];then
-  curl -L https://github.com/Xhofe/alist/releases/latest/download/alist-linux-amd64.tar.gz -o alist.tar.gz
+  curl -L https://github.com/Xhofe/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
   tar -zxvf alist.tar.gz
   rm -f alist.tar.gz
-  mv alist-linux-amd64 alist
+  mv alist-linux-musl-amd64 alist
 fi
 ./alist -docker
