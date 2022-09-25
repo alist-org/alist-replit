39925f0ab8506c158e3ed448eb817424ad4cc1f4
Noah Hsu <i@nn.ci>
2022-09-25 20:52:19 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,7 @@
 # rm -rf alist* data/ #Uncomment this line to update
 if [ ! -f "alist" ];then
-  curl -L https://github.com/alist-org/alist/releases/download/v2.6.4/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
+  curl -L https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz -o alist.tar.gz
   tar -zxvf alist.tar.gz
   rm -f alist.tar.gz
-  mv alist-linux-musl-amd64 alist
 fi
-./alist -docker
+./alist server --no-prefix
